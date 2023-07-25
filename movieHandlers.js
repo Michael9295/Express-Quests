4053d4afa1eeeb974697d9609d302da205cd38d3
Michael9295 <auramichael.9pgmail.com>
2023-07-25 16:27:25 +0200

    first commit

---
+++ movieHandlers.js
@@ -0,0 +1,42 @@
+const database = require("./database");
+
+const getMovies = (req, res) => {};
+
+const getMovieById = (req, res) => {};
+
+const getUsers = (req, res) => {
+  database
+    .query("SELECT * FROM users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("SELECT * FROM users WHERE id = ?", [id])
+    .then(([users]) => {
+      if (users.length > 0) {
+        res.status(200).json(users[0]);
+      } else {
+        res.status(404).json({ message: "Not Found" });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+module.exports = {
+  getMovies,
+  getMovieById,
+  getUsers,
+  getUserById,
+};
